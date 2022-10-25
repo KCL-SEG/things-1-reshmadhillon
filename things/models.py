16ee162551606af788911fd0ca6d9a3b3920279f
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-25 14:04:47 +0100

    changing validation in model

--- things/models.py
+++ things/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 class Thing(Model):
     name = models.CharField(
-        max_length=50,
-        blank = False
+        max_length=30,
+        unique=True
     )
     description = models.CharField(
-        max_length=250,
+        max_length=100,
         blank = False
     )
     quantity = models.IntegerField(
-        blank = False
+        blank = False,
+        validators=[MaxValueValidator(100),
+                    MinValueValidator(0)]
+        #test_quantity_must_not_be_greater_than_100,
+        #test_quantity_must_not_be_negative
     )
