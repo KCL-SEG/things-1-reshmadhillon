6f5a131a242dca4fd0b6d37b317e7f7f1dded645
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-25 14:13:25 +0100

    Description can be blank

--- things/models.py
+++ things/models.py
@@ -8,7 +8,8 @@ class Thing(Model):
         unique=True
     )
     description = models.CharField(
-        max_length=120
+        max_length=120,
+        blank = True
     )
     quantity = models.IntegerField(
         validators=[MaxValueValidator(100),
