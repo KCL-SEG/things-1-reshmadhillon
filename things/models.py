cd6bcb9768b02dc5b987d6a9b4115159228fc165
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-25 10:52:53 +0100

    Making model and migrations

--- things/models.py
+++ things/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(Model):
+    name = models.CharField(
+        max_length=50,
+        blank = False
+    )
+    description = models.CharField(
+        max_length=250,
+        blank = False
+    )
+    quantity = models.IntegerField(
+        blank = False
+    )
